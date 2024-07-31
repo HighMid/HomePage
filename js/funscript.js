0543c624321b0457f2541bc9bce33bff066589fa
HighMid <xstar671@gmail.com>
2024-07-31 19:19:41 -0400

    Updated Js file

--- js/funscript.js
+++ js/funscript.js
@@ -1,9 +1,9 @@
-
+// funscript.js
 window.onload = function() {
     let major = prompt("What is your major?");
     if (major) {
-        document.write("Great, we love " + major + " majors in this club!");
+        alert("Great, we love " + major + " majors in this club!");
     } else {
-        document.write("Great, we love all majors in this club!");
+        alert("Great, we love all majors in this club!");
     }
 };
